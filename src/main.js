3c71c01df5f40e65f688ba637ccda6ae457afe45
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2024-12-12 11:52:04 +0100

    Apend the container div dom element to the rendrer and change the postion of our camera to we can see the element and rendrer our scene

--- src/main.js
+++ src/main.js
@@ -12,6 +12,13 @@ const camera = new THREE.PerspectiveCamera(
 );
 const renderer = new THREE.WebGLRenderer();
 
+// Set renderer size
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Append renderer to container
+const container = document.getElementById("solar-system");
+container.appendChild(renderer.domElement);
+
 // Inisialize Geometry, Material, Mesh
 const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
 const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
@@ -19,3 +26,9 @@ const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
 // Add sphere to scene
 scene.add(sphere);
+
+// Set camera position
+camera.position.z = 5;
+
+// Render the scene
+renderer.render(scene, camera);
